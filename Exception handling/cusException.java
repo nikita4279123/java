1fc0ac00032246fa1d31cc20c2d0c381798b114a
nikita4279123 <nikitamunda42@gmail.com>
2024-08-30 10:31:53 +0000

    cusException

--- Exception handling/cusException.java
+++ Exception handling/cusException.java
@@ -1,11 +1,20 @@
+import javax.lang.model.type.MirroredTypeException;
+
 /**
  * cusException
  */
 public class cusException {
     void show(int x)throws MyException{
         if (x>40)
-        
-    }
+        throw new MyException("value is >20");
+        else
+    System.out.println(x*x);
+}
+public static void main(String[] args) {
+    cusException ce =new cusException();
+    
+}
+
 
     
 }
